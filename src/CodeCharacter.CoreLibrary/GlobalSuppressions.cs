dfd5043eb9326974eccebded45b457b96d8ba2d6
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2021-12-08 00:37:27 +0530

    Update lints

---
+++ src/CodeCharacter.CoreLibrary/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1834:Consider using 'StringBuilder.Append(char)' when applicable", Justification = "Not needed")]
+[assembly: SuppressMessage("Style", "IDE0070:Use 'System.HashCode'", Justification = "Not needed")]
+[assembly: SuppressMessage("Style", "IDE0057:Use range operator", Justification = "Not needed")]
+[assembly: SuppressMessage("Performance", "CA1829:Use Length/Count property instead of Count() when available", Justification = "Not needed")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Not needed")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Not needed")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Not needed")]
+[assembly: SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "Not needed")]
